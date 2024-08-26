f14a92fd1e8a3b908b762b57a933e84acfcd6152
Matheus Silva <matheus.vunk@gmail.com>
2024-08-26 15:11:42 -0300

    first commit

---
+++ src/test/java/vunk/dio/dio_claro_bootcamp_restful_api/DioClaroBootcampRestfulApiApplicationTests.java
@@ -0,0 +1,13 @@
+package vunk.dio.dio_claro_bootcamp_restful_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DioClaroBootcampRestfulApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
