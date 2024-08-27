6fe329bd5edd5f13437b8f6bd9617ced4549ab31
Matheus Silva <matheus.vunk@gmail.com>
2024-08-27 16:38:33 -0300

    fix cors

--- src/main/java/vunk/dio/restfulapi/App.java
+++ src/main/java/vunk/dio/restfulapi/App.java
@@ -3,6 +3,10 @@ package vunk.dio.restfulapi;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
+
+@OpenAPIDefinition(servers = {  @Server(url = "/", description =  "Url padrão do server") })
 @SpringBootApplication
 public class App {
 
